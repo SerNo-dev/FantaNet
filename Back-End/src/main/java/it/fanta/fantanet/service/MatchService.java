c4d41cc5b022ecfcd8fbc9846645bd5053bba17b
Sergio <131872447+SerNo-dev@users.noreply.github.com>
2024-06-28 10:29:12 +0200

    buttons and component replay correction

--- Back-End/src/main/java/it/fanta/fantanet/service/MatchService.java
+++ Back-End/src/main/java/it/fanta/fantanet/service/MatchService.java
@@ -15,8 +15,17 @@ public class MatchService {
 
     public void saveMatch(Match match) {
         System.out.println("Saving match to database: " + match);
+
+        // Arrotonda i punteggi prima di salvare
+        match.setUser1Score(roundScore(match.getUser1Score()));
+        match.setUser2Score(roundScore(match.getUser2Score()));
+
         matchRepository.save(match);
         System.out.println("Match saved to database: " + match);
+    }
+
+    private double roundScore(double score) {
+        return Math.round(score * 100.0) / 100.0; // Arrotonda a due cifre decimali
     }
 
     public List<Match> getMatchesByUser(Utente user) {
