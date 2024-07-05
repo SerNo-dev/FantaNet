2653d9b1cde5619fc8d79cf4f3026a3988887fe2
Sergio Nolasco <intell@hotmail.it>
2024-07-05 15:28:55 +0200

    Spinner

--- Front-End/src/app/components/dashboard/dashboard.component.ts
+++ Front-End/src/app/components/dashboard/dashboard.component.ts
@@ -7,15 +7,38 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.showLoader();
+
     const token = this.authService.getToken();
     if (!token) {
-      // Gestisci il caso in cui il token non è presente (ad es. reindirizza al login)
+    
       console.log('Token non presente, reindirizza al login');
+      this.hideLoader();
     } else {
       console.log('Token:', token);
+
+      setTimeout(() => {
+        this.hideLoader();
+      }, 2000); 
+    }
+  }
+
+  showLoader(): void {
+    const loader = document.getElementById('loader');
+    if (loader) {
+      loader.style.display = 'flex';
+    }
+  }
+
+  hideLoader(): void {
+    const loader = document.getElementById('loader');
+    if (loader) {
+      loader.style.display = 'none';
     }
   }
 }
