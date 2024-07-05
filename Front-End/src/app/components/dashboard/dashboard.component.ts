66699197bb0637c32ab54688f9df8d9d313799b4
Sergio Nolasco <intell@hotmail.it>
2024-07-05 16:25:32 +0200

    last update

--- Front-End/src/app/components/dashboard/dashboard.component.ts
+++ Front-End/src/app/components/dashboard/dashboard.component.ts
@@ -7,38 +7,14 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-
-
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.showLoader();
-
     const token = this.authService.getToken();
     if (!token) {
-    
       console.log('Token non presente, reindirizza al login');
-      this.hideLoader();
     } else {
       console.log('Token:', token);
-
-      setTimeout(() => {
-        this.hideLoader();
-      }, 2000); 
-    }
-  }
-
-  showLoader(): void {
-    const loader = document.getElementById('loader');
-    if (loader) {
-      loader.style.display = 'flex';
-    }
-  }
-
-  hideLoader(): void {
-    const loader = document.getElementById('loader');
-    if (loader) {
-      loader.style.display = 'none';
     }
   }
 }
